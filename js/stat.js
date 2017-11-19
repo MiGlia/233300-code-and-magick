e60bbafe53da7cc6a8819d17fabe03b42694046b
MishaGl <mishaglansev@gmail.com>
2017-11-19 22:08:10 +0300

    Вывод экрана статистики

--- js/stat.js
+++ js/stat.js
@@ -1,13 +1,46 @@
 'use strict';
 
 // Статистика игры после того как игрок выиграл
-// ctx - Canvas Rendering Context
 // names - Имена игроков
 // times - Время игры.
 
 window.renderStatistics = function (ctx, names, times) {
 
-// Рисуем окно для вывода статистики
+  // Находим максимальное число в массиве
+  function getMaxElement(array) {
+    return array.slice().sort(function (a, b) {
+      return a - b;
+    })[([array.length - 1])];
+  }
+
+  //  Рисуем гистограмму
+  function drawTheHistogram() {
+    var maxTime = getMaxElement(times);
+    var step = hist.height / maxTime; // px;
+    var initialX = 120; // px;
+    var initialY = 100; // px;
+    var lineHeight = 20; // px;
+
+    for (var i = 0; i < times.length; i++) {
+
+      var indentX = (hist.indent + hist.width) * i;
+      var indentY = hist.height - times[i] * step;
+
+      ctx.fillStyle = (names[i] === 'Вы') ? 'rgba(255, 0, 0, 1.0)' : 'rgba(25, 61, 224,  ' + Math.random() + ')';
+      ctx.fillRect(initialX + indentX, initialY + indentY, hist.width, times[i] * step);
+      ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
+      ctx.fillText(names[i], initialX + indentX, hist.height + initialY + lineHeight);
+      ctx.fillText(Math.floor(times[i]), initialX + indentX, initialY + indentY - lineHeight);
+    }
+  }
+
+  var hist = {
+    width: 40,
+    height: 150,
+    indent: 50
+  };
+
+  // Рисуем окно для вывода статистики
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
@@ -21,42 +54,5 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
-
-  //  Сортируем массив времен
-  var getMaxElement = function (array) {
-    for (var i = 0; i <= times.length - 1; i++) {
-      var minValue = times[i];
-
-      for (var j = i + 1; j <= times.length - 1; j++) {
-        if (times[j] < minValue) {
-          minValue = times[j];
-          var swap = times[i];
-          times[i] = minValue;
-          times[j] = swap;
-        }
-      }
-    }
-    return (times[times.length - 1]);
-  };
-
-  var histogramHeight = 150; // px;
-  var histogramWidth = 40; // px;
-  var step = histogramHeight / getMaxElement(); // px;
-  var indent = 50; // px;
-  var initialX = 120; // px;
-  var initialY = 100; // px;
-  var lineHeight = 20; // px;
-
-
-  // Строти гистограмму
-  for (var i = 0; i < times.length; i++) {
-    var indentX = (indent + histogramWidth) * i;
-    var indentY = histogramHeight - times[i] * step;
-
-    ctx.fillStyle = (names[i] === 'Вы') ? 'rgba(255, 0, 0, 1.0)' : 'rgba(25, 61, 224,  ' + Math.random() + ')';
-    ctx.fillRect(initialX + indentX, initialY + indentY, histogramWidth, times[i] * step);
-    ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
-    ctx.fillText(names[i], initialX + indentX, histogramHeight + initialY + lineHeight);
-    ctx.fillText(Math.floor(times[i]), initialX + indentX, initialY + indentY - lineHeight);
-  }
+  drawTheHistogram();
 };
