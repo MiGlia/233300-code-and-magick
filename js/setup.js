d33c3149bab3e4141e005e5135d0ea5ce6cae1d8
MishaGl <mishaglansev@gmail.com>
2017-11-23 01:13:48 +0300

    Вывод экрана с персонажем

--- js/setup.js
+++ js/setup.js
@@ -1,22 +1,24 @@
 'use strict';
 
+// Создаем массивы
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
-
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
-document.querySelector('.setup-similar').classList.remove('hidden');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+var similarListElement = document.querySelector('.setup-similar-list');
 
 // Находим случайный элемент в массиве
 function getRandomArrayIndex(Array) {
   return Array[Math.floor(Math.random() * Array.length)];
 }
 
-// Генерируем массив с параметрами мага: name, coatColor, eyesColor.
+// Генерируем массив объектов с параметрами мага: name, coatColor, eyesColor.
 var wizards = [
   {
     name: getRandomArrayIndex(WIZARD_NAMES) + ' ' + getRandomArrayIndex(WIZARD_SURNAMES),
@@ -40,9 +42,7 @@ var wizards = [
   }
 ];
 
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
-var similarListElement = document.querySelector('.setup-similar-list');
-
+// Копируем шаблон(идеальный элемент) и заполняем его новыми данными(name, coatColor, eyesColor)
 function renderWizard(wizards) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
@@ -52,6 +52,7 @@ function renderWizard(wizards) {
   return wizardElement;
 }
 
+//  Группируем элементы в обвертку fragment, Вставляем заполненные элементы в DOM и отрсовываем их
 function getRenderWizard() {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < wizards.length; i++) {
@@ -60,17 +61,5 @@ function getRenderWizard() {
   similarListElement.appendChild(fragment);
 }
 getRenderWizard();
+
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
-// var similarListElement = document.querySelector('.setup-similar-list');
-// var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
-//
-// for (var i = 0; i < wizards.length; i++) {
-//   var wizardElement = similarWizardTemplate.cloneNode(true);
-//
-//   wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-//   wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-//   wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
-//
-//   similarListElement.appendChild(wizardElement);
-// }
-// userDialog.querySelector('.setup-similar').classList.remove('hidden');
